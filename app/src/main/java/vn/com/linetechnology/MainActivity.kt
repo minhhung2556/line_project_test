5dcacf08ae0551cf072b4c0a7124d1d67b8c40c6
Hung Luong Do Minh <minhhung2556@gmail.com>
2020-08-04 22:44:25 +0700

    read and set title to view

--- app/src/main/java/vn/com/linetechnology/MainActivity.kt
+++ app/src/main/java/vn/com/linetechnology/MainActivity.kt
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import vn.com.linetechnology.data.SampleMovie
+import vn.com.linetechnology.data.sampleJsonData
 
 class MainActivity : AppCompatActivity() {
+    var model: SampleMovie? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+        setData()
+    }
+
+    private fun setData() {
+        tv_movie_title.text = model?.title ?: ""
+    }
+
+    private fun loadData() {
+        model = SampleMovie.fromJson(sampleJsonData)
+        loadImage()
+    }
+
+    private fun loadImage() {
     }
 }
