5d7ca88957e7fbaa7d2d7817cb489149828ba24b
Hung Luong Do Minh <minhhung2556@gmail.com>
2020-08-05 02:09:11 +0700

    implement logic set & change image

--- app/src/main/java/vn/com/linetechnology/MainActivity.kt
+++ app/src/main/java/vn/com/linetechnology/MainActivity.kt
@@ -1,13 +1,19 @@
 package vn.com.linetechnology
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import vn.com.linetechnology.data.SampleMovie
 import vn.com.linetechnology.data.sampleJsonData
+import vn.com.linetechnology.image.ImageLoader
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     var model: SampleMovie? = null
+    val imageLoader = ImageLoader()
+    var currentImage: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +33,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImage() {
+        model?.image?.forEach { img ->
+            imageLoader.load(baseContext, img, { percent: Int, byteLen: Int ->
+                tv_progress.text = "$percent - $byteLen"
+            }, { bitmap: Bitmap? ->
+                im_movie_image.setImageBitmap(bitmap)
+            })
+        }
+    }
+
+    private fun changeImage() {
+        val list = model!!.image!!.asSequence().toList()
+        if (currentImage < list.size - 1)
+            currentImage++
+        else currentImage = 0
+        val img = list[currentImage]
+
+        imageLoader.load(baseContext, img, { percent: Int, byteLen: Int ->
+            tv_progress.text = "$percent - $byteLen"
+        }, { bitmap: Bitmap? ->
+            im_movie_image.setImageBitmap(bitmap)
+        })
+    }
+
+    fun onClickOnImage(view: View) {
+        changeImage()
     }
 }
