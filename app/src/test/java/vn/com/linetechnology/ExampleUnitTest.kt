6aef4f75230506928ac3c92b485462f871c1a426
Hung Luong Do Minh <minhhung2556@gmail.com>
2020-08-04 22:31:54 +0700

    unit test parse sample data

--- app/src/test/java/vn/com/linetechnology/ExampleUnitTest.kt
+++ app/src/test/java/vn/com/linetechnology/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package vn.com.linetechnology
 import org.junit.Test
 
 import org.junit.Assert.*
+import vn.com.linetechnology.data.SampleMovie
+import vn.com.linetechnology.data.sampleJsonData
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,11 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun parseJsonIsCorrect() {
+        val m = SampleMovie.fromJson(sampleJsonData)
+        assertNotNull(m)
+        assertNotNull(m?.title)
+        assertNotEquals(m?.image?.size ?: 0, 0)
+        assertNotNull(m?.image?.get(0))
     }
 }
