516640abcb95b50b0035fa33d012b333177af561
hungldm <hungldm@sendo.vn>
2020-08-05 11:45:50 +0700

    remove unused code

--- app/src/main/java/vn/com/linetechnology/image/ImageDecoder.kt
+++ app/src/main/java/vn/com/linetechnology/image/ImageDecoder.kt
@@ -48,19 +48,3 @@ fun getBitmap(file: File, screenW: Int, screenH: Int): Bitmap? {
         null
     }
 }
-
-fun getScaledBitmap(bm: Bitmap, newHeight: Int, newWidth: Int): Bitmap? {
-    val width = bm.width
-    val height = bm.height
-    val scaleWidth = newWidth.toFloat() / width
-    val scaleHeight = newHeight.toFloat() / height
-    // CREATE A MATRIX FOR THE MANIPULATION
-    val matrix = Matrix()
-    // RESIZE THE BIT MAP
-    matrix.postScale(scaleWidth, scaleHeight)
-    // "RECREATE" THE NEW BITMAP
-    return Bitmap.createBitmap(
-        bm, 0, 0, width, height,
-        matrix, false
-    )
-}
